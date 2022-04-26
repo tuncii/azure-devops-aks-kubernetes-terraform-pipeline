8123047e8b08818c53a17f8b8843283088023b1b
tuncii <celiktunch@gmail.com>
2022-04-26 12:39:13 +0300

    AD fixed

--- terraform-manifests/06-aks-administrators-azure-ad.tf
+++ terraform-manifests/06-aks-administrators-azure-ad.tf
@@ -2,6 +2,6 @@
 resource "azuread_group" "aks_administrators" {
   #name        = "${azurerm_resource_group.aks_rg.name}-administrators"
   display_name        = "${azurerm_resource_group.aks_rg.name}-${var.environment}-administrators"
-  description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-${var.environment} cluster."
+  description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name} -administrators cluster"
   security_enabled = true
 }
